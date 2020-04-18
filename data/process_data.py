a7477ce538f149de2c8bc0c8b03a9766eb143600
julian <julian@dnlab.de>
2020-04-18 10:31:20 +0200

    initializing project

---
+++ data/process_data.py
@@ -0,0 +1,75 @@
+import sys
+import pandas as pd
+import numpy as np
+import re
+from sqlalchemy import create_engine
+
+
+def load_data(messages_filepath, categories_filepath):
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath, sep =",")
+    df = messages.merge(categories, how = "left", on = "id")
+    return df, categories
+def clean_data(df, categories):
+    # create a dataframe of the 36 individual category columns
+    categories = categories.categories.str.split(";", expand = True)
+
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+    category_colnames = [re.split("-",word)[0] for word in row.tolist()]
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    # Get the values from the last character of the string
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x[-1])
+        # convert column from string to numeric
+        categories[column] = categories[column].apply(int)
+
+    # Drop col
+    df.drop("categories", axis = 1, inplace = True)
+    df = df.merge(categories, how = "inner", left_index = True, right_index = True)
+    
+    # Drop duplicates
+    df = df.drop_duplicates("id")
+    return df
+    
+
+def save_data(df, database_filename):
+
+    # Create db
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql('emergency', engine, index=False)
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df, categories = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df, categories)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/train_classifier.py
@@ -0,0 +1,125 @@
+import time
+import sys
+from sqlalchemy import create_engine
+import pandas as pd
+from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords 
+import string
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer,TfidfTransformer
+import numpy as np
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from sklearn.naive_bayes import MultinomialNB
+import pickle
+
+# Detect the language of only the first comment
+nltk.download('stopwords')
+nltk.download('punkt')
+language = "english"
+STOPWORDS = set(stopwords.words(language))
+PUNCT_TO_REMOVE = string.punctuation
+
+
+
+def remove_punctuation(text):
+    """custom function to remove the punctuation"""
+    return text.translate(str.maketrans('', '', PUNCT_TO_REMOVE))    
+
+
+# Stemmer
+stemmer = PorterStemmer()
+def stem_word(word):
+    try:
+        return stemmer.stem(word)
+    except RecursionError:
+        return ""
+    
+
+def load_data(database_filepath):
+    # load data from database
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql_table("emergency", engine)
+    X = df.message.values
+    y = df.loc[:,"related":].values    
+    return X,y
+
+
+def tokenize(text):
+    word_list = word_tokenize(text)
+    word_list = [word.lower() for word in word_list]
+    word_list = [word for word in word_list if word not in STOPWORDS]
+    word_list = [remove_punctuation(word) for word in word_list if len(word)>1]
+    word_list = [stem_word(word) for word in word_list if len(word)>1]
+    text = " ".join(word_list)
+    return text
+
+
+def build_model():
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ("clf", MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    param_grid = {
+        'vect__max_df':[1.0],
+        'vect__min_df':[1],        
+        'vect__max_features':[1000],
+#        'tfidf__use_idf':(True,False),
+    }
+
+    model = GridSearchCV(
+        pipeline,
+        param_grid,
+        cv=5,
+        n_jobs=-1,
+        verbose=2,
+    )    
+        
+    return model
+
+def evaluate_model(model, X_test, Y_test):
+    pass
+
+
+def save_model(model, model_filepath):
+    with open(f'{model_filepath}', 'wb') as handle:
+        pickle.dump(model, handle)
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
